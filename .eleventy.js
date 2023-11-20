7a63ac49aca8be569a54d949de40eb1f7023061c
Aldo Joyce <143909435+TravellerEntity@users.noreply.github.com>
2023-11-20 18:33:00 +0000

    Further config of site build

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
 const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/**");
+  //Always include src/ before file paths!
+  eleventyConfig.addPassthroughCopy("src/assets/**");
+  eleventyConfig.addPassthroughCopy("src/demos/**");
+  eleventyConfig.addPassthroughCopy("src/fonts/**");
+  eleventyConfig.addPassthroughCopy("src/javascripts/**");
+  eleventyConfig.addPassthroughCopy("src/*.html");
+  eleventyConfig.addPassthroughCopy("src/*.css");
+  eleventyConfig.addPassthroughCopy("src/*.js");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.setQuietMode("true");
   eleventyConfig.addPlugin(directoryOutputPlugin);
   return {
---
+++ src/blog/helloworld.md
@@ -0,0 +1,22 @@
+---
+title: Hello World
+layout: post
+dateText: Nov 11, 2023
+date: 2023-11-20
+---
+# Hello World
+Lorem ipsum dolor sit amet
+```
+# A wild codeblock appears
+for i in range(5):
+    print("Printed "+str(i))
+```
+According to all known laws of aviation, there is no way a bee should be able to fly. Its wings
+are too small to get its fat little body off the ground. The bee, of course, flies anyway because
+bees don't care what humans think is impossible.
+
+Never gonna give you up
+
+Never gonna let you down
+
+Never gonna run around and desert you

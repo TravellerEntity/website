b5d16e69e7426818f1c04aedc7c556ee226822ba
Aldo Joyce <143909435+TravellerEntity@users.noreply.github.com>
2024-01-12 21:28:07 +0000

    Add JackCode

--- src/projects/encode/encode.js
+++ src/projects/encode/encode.js
@@ -1,4 +1,4 @@
-const charmap = {
+const jonasmap = {
     "a": 21,
     "b": 35,
     "c": 33,
@@ -30,6 +30,39 @@ const charmap = {
     "!": 98,
     " ": 99
 };
+const jackmap = {
+    "a": 1107,
+    "b": 19,
+    "c": 18,
+    "d": 17,
+    "e": 16,
+    "f": 15,
+    "g": 14,
+    "h": 13,
+    "i": 12,
+    "j": 11,
+    "k": 201,
+    "l": 210,
+    "m": 209,
+    "n": 208,
+    "o": 207,
+    "p": 206,
+    "q": 205,
+    "r": 204,
+    "s": 203,
+    "t": 202,
+    "u": 3001,
+    "v": 3100,
+    "w": 3099,
+    "x": 3089,
+    "y": 3079,
+    "z": 111,
+    ".": 4000,
+    "?": 3999,
+    "!": 5000,
+    " ": 42
+};
+var currentMode = "jonas";
 
 var inputarea = document.getElementById("input");
 var outputarea = document.getElementById("output");
@@ -43,12 +76,25 @@ function disableCopy() {
     }
 }
 
+function switchEncode() {
+    header = document.querySelector("h1");
+    if (currentMode == "jonas") {
+        currentMode = "jack";
+        header.innerText = "JackCode Encoder";
+    } else {
+        currentMode = "jonas";
+        header.innerText = "JonasCode Encoder"
+    }
+    encode();
+}
+
 function encode() {
     var input = Array.from(inputarea.value.toLowerCase());
     var output = "";
+    var charMap = "jonas" == currentMode ? jonasmap : jackmap;
     for (var i=0; i<input.length; i++) {
-        if (input[i] in charmap) {
-            var character = charmap[input[i]].toString();
+        if (input[i] in charMap) {
+            var character = charMap[input[i]].toString();
             output += character;
         }
     }

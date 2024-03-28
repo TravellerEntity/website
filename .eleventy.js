7b4f8d305510be2539430d7d73284ca485c45422
Aldo Joyce <143909435+TravellerEntity@users.noreply.github.com>
2024-03-28 12:58:15 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,18 +3,16 @@ module.exports = function(eleventyConfig) {
   //Always include src/ before file paths!
   eleventyConfig.addPassthroughCopy("src/assets/**");
   eleventyConfig.addPassthroughCopy("src/admin/**");
-  eleventyConfig.addPassthroughCopy("src/projects/demos/**");
   eleventyConfig.addPassthroughCopy("src/fonts/**");
   eleventyConfig.addPassthroughCopy("src/projects/**");
   eleventyConfig.addPassthroughCopy("src/blog/post/post.css");
   eleventyConfig.addPassthroughCopy("src/*.html");
   eleventyConfig.addPassthroughCopy("src/*.css");
   eleventyConfig.addPassthroughCopy("src/*.js");
-  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/*.ico");
   eleventyConfig.setQuietMode("true");
   eleventyConfig.addPlugin(directoryOutputPlugin);
   return {
-    passthroughFileCopy: true,
     dir: {
       input: "src",
       output: "site"
